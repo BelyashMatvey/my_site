df4db41c172637cb5981a80013a9bc691a6ab590
BelyashMatvey <belyakov06mat@mail.ru>
2021-09-27 14:14:04 +0300

    final database of drums and full layout of home

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
@@ -11,14 +12,20 @@ class Article(db.Model):
     fname=db.Column(db.String(50),primary_key=True,nullable=False)
     sname=db.Column(db.String(50),primary_key=True,nullable=False)
     date=db.Column(db.DateTime,default=datetime.utcnow())
+    link=db.Column(db.String(100),nullable=False)
+    link_image=db.Column(db.String(100),nullable=False)
     mark=db.Column(db.Float,nullable=False)
     number=db.Column(db.Integer,primary_key=True,nullable=False)
     def __repr__(self):
         return '<Article %r>' % self.id
 @app.route('/')
+@app.route('/log_in')
+def log_in():
+    return render_template('sign_in.html')
 @app.route('/home')
 def home():  # put application's code here
-    return render_template('index.html')
+    articles=Article.query.order_by(Article.mark.desc()).all()
+    return render_template('index.html',articles=articles)
 @app.route('/create-article',methods=['POST','GET'])
 def create_article():
     if request.method=="POST":
@@ -27,11 +34,12 @@ def create_article():
         fname = request.form["fname"]
         sname = request.form["sname"]
         name_music= request.form["name_music"]
-        article=Article(mark=mark,number=number,fname=fname,name_music=name_music,sname=sname)
+        link=request.form["link"]
+        link_image="https://i.ytimg.com/vi/"+link[17:]+"/mqdefault.jpg"
+        article=Article(mark=mark,number=number,fname=fname,name_music=name_music,sname=sname,link=link,link_image=link_image)
         db.session.add(article)
-        print("норм")
         db.session.commit()
-        print("норм")
+        print(link_image)
         return redirect('/home')
     else:
         return render_template('create-article.html')
